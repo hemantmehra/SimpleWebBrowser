b2791c64ecd64c4fe75a732e2345cf95c53cf0a0
Hemant Mehra <mehra.hemant14@gmail.com>
2022-08-15 16:54:31 +0530

    Program can now read html content from file provided in commandline argument.
    Create sample folder with html sample files.

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,36 @@
 #include <iostream>
-#include <HTMLParser.h>
+#include <sstream>
+#include <fstream>
 #include <Dump.h>
+#include <HTMLParser.h>
 
 int main(int argc, char const *argv[])
 {
-    std::string source = "<!DOCTYPE html><html><head><meta content=\"Hello There!\"></head><body><div></div></body></html>";
+
+    if (argc < 2) {
+        std::cout << "Simple Web Browser" << std::endl;
+        std::cout << std::string(50, '-') << std::endl;
+        std::cout << "Usage swb <filename>" << std::endl;
+        return 0;
+    }
+
+    // Read html file
+    std::string filename = argv[1];
+    std::ifstream file_stream(filename);
+    std::stringstream buffer;
+    while(file_stream >> buffer.rdbuf());
+
+    // Print source
+    std::string source = buffer.str();
     std::cout << source << std::endl;
     std::cout << std::string(50, '-') << std::endl;
 
+    // Parse source
     Web::HTMLParser html_parser(source);
     html_parser.run();
     std::cout << std::string(50, '-') << std::endl;
 
+    // Print parse tree
     std::string s;
     Web::dump_tree(s, html_parser.document(), 0);
     std::cout << s << std::endl;
